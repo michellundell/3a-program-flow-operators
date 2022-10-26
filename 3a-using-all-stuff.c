a9422b5259ef34fae9933c79c8e240d48b3b0f95
Mohamud74 <116171848+Mohamud74@users.noreply.github.com>
2022-10-26 15:57:54 +0200

    Update 3a-using-all-stuff.c
    New Changes

--- 3a-using-all-stuff.c
+++ 3a-using-all-stuff.c
@@ -1,3 +1,11 @@
+/**
+ * @file test.c
+ * @author Mohamud Abdi(mohamud.abdi@studerande.movant.se)
+ * @brief  compares two numbers if they are the same printed true otherwise false
+ * @version 0.1
+ * @date 2022-10-26
+ * 
+ */
 #include <stdio.h>
 
 /*
@@ -6,8 +14,53 @@
 ** and how to use the ! 
 */
 
+
+/**
+ * @brief 
+ * 
+ */
+
+/**
+ * @brief 
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
 int main(int argc, char **argv) 
 {
+ int x,y;
 
-	return 0;
+    for(int b=0;b<argc; b++) {
+        printf("argv[%d] = %s\n",b,argv[b]);
+    }
+// compares two numbers if they are the same printed true otherwise false
+
+
+printf ( "write your first number");
+  scanf("%d",&x );
+ printf ( "write your second number");
+  scanf("%d",&y);
+ 
+ int sum = x+y;
+ 
+ 
+  if(x==y ) {
+ printf("%d == %d is true\n",x,y);
+  }  else if(x!=y) {
+ printf("%d ==%d is false \n" ,x,y);
 }
+
+for(int x=y; y<x; y--) {
+ 
+}
+ printf("\n");
+ 
+ 
+ 
+ 
+
+
+
+return 0;
+} 
