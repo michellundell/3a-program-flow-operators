82a8e070bd4b83933bb958ce25da6e7c59d53d2c
Jesper Rehnfors <rehnforsjesper@gmail.com>
2022-10-26 12:13:30 +0200

    solved assignment

--- 3a-using-all-stuff.c
+++ 3a-using-all-stuff.c
@@ -6,8 +6,51 @@
 ** and how to use the ! 
 */
 
-int main(int argc, char **argv) 
+// Ett program som testar allt ovan, ganska meningslöst men kul program 
+int main(int argc, char **argv)
 {
+    char again = 'y';
 
-	return 0;
+    do
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            switch (i)
+            {
+            case 0:
+                printf("-Hello!");
+                break;
+            case 1:
+                printf("how ");
+                break;
+            case 2:
+                printf("are ");
+                break;
+            case 3:
+                printf("you ");
+                break;
+            default:
+                printf("? ");
+            }
+		
+            for (int j = 0; j <= 5 || ( !1 ); j++)
+            {
+                if (j >= i)
+                {
+                    printf("\n");
+                }
+				if ( ! j==1 )
+				{
+					printf(":)");
+				}
+				else
+				{
+					printf("-");
+				}
+            }
+        }
+        printf("Would you like to go again? (y/n)");
+        scanf(" %c", &again);
+    } while (again != 'n');
+    return 0;
 }
