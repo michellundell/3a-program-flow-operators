5a2c2b7bdff1bb4b1010736abb3035e59b1b91d4
Wayazaexa <joakimenglund@protonmail.com>
2022-11-02 16:26:05 +0100

    Forgot to commit, but I added comments and doxygen-comments.

--- 3a-using-all-stuff.c
+++ 3a-using-all-stuff.c
@@ -1,11 +1,24 @@
+/**
+ * @file 3a-using-all-stuff.c
+ * @author Joakim Englund (joakimenglund@protonmail.com)
+ * @brief This program will use if, for, while, do, switch statements. 
+ * It should also demonstrate the use of comparisons ==, !=, <=, >= 
+ * and how to use the ! 
+ * @version 1.0
+ * @date 2022-10-26
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <stdio.h>
 
-/*
-** This program will use if, for, while, do, switch statements. 
-** It should also demonstrate the use of comparisons ==, !=, <=, >= 
-** and how to use the ! 
-*/
-
+/**
+ * @brief This is the main function, where all the magic happens.
+ * 
+ * @param argc Number of command line arguments.
+ * @param argv An array of the command line arguments as strings.
+ * @return returns 0 on successful run.
+ */
 int main(int argc, char **argv) 
 {
 	int x = 2, y = 7;
@@ -23,6 +36,10 @@ int main(int argc, char **argv)
 	}
 	printf("\n");
 
+	/**
+	 * @brief There's a very complex for loop here, doing very important things!
+	 * 
+	 */
 	printf("for-loop (i = 0; i < 10; i += 2)):\n");
 	for (int i = 0; i < 10; i += 2)
 	{
@@ -45,6 +62,7 @@ int main(int argc, char **argv)
 	printf("\n");
 	y = 7;
 
+	// do-while with a switch-case statement inside.
 	printf("do {  } while (x <= 10):\n");
 	do
 	{
@@ -57,6 +75,7 @@ int main(int argc, char **argv)
 			case 2:
 			case 3:
 			case 4:
+				// Anything between 0 and 4 will print this.
 				printf("\t\t0 <= x <= 4\n");
 				break;
 			case 5:
@@ -67,9 +86,11 @@ int main(int argc, char **argv)
 				break;
 			case 7:
 			case 8:
+				// This will print for both 7 and 8.
 				printf("\t\tx = 7 or 8\n");
 			case 9:
 			case 10:
+				// Anything between 7 and 10 will print this.
 				printf("\t\t7 <= x <= 10\n");
 				break;
 			case 11:
